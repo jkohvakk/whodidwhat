456453b640aa22a9f9fa0b647b47b6572ca2d779
Janne Kohvakka <jkohvakk@gmail.com>
2015-08-20 11:40:42 +0300

    Direct reading of svn repos

--- test/test_svn_filter.py
+++ test/test_svn_filter.py
@@ -4,23 +4,29 @@ import StringIO
 import filecmp
 from whodidwhat.svnfilter import SvnFilter
 
+from mock import patch
+
 MODULE_DIR = os.path.dirname(__file__)
 
 
 class TestFilterSvn(unittest.TestCase):
 
     def setUp(self):
-        self.svn_xml = os.path.join(MODULE_DIR, 'sample_svn.xml')
+        self.svn_xml = os.path.join(MODULE_DIR, 'sample_data', 'sample_svn.xml')
+        with open(self.svn_xml) as sample_xml_file:
+            self.svn_xml_text = sample_xml_file.read()
+        self.usersfile = os.path.join(MODULE_DIR, 'sample_data', 'userlist.txt')
+        self.output_xml = os.path.join(MODULE_DIR, 'output.xml')
         self.log_filter = SvnFilter()
 
     def test_filtering_log_for_one_user(self):
-        _, entries = self.log_filter.get_logs_by_users(self.svn_xml, ['jkohvakk'])
+        _, entries = self.log_filter.get_logs_by_users(self.svn_xml_text, ['jkohvakk'])
         self.assertEqual(1, len(entries))
         self.assertEqual('213', entries[0].attrib['revision'])
         self.assertEqual('jkohvakk', entries[0].find('author').text)
 
     def test_filtering_log_for_two_users(self):
-        _, entries = self.log_filter.get_logs_by_users(self.svn_xml, ['kmikajar', 'jkohvakk'])
+        _, entries = self.log_filter.get_logs_by_users(self.svn_xml_text, ['kmikajar', 'jkohvakk'])
         self.assertEqual(2, len(entries))
         self.assertEqual('210', entries[0].attrib['revision'])
         self.assertEqual('kmikajar', entries[0].find('author').text)
@@ -40,13 +46,23 @@ basvodde
                          self.log_filter.read_userlist(StringIO.StringIO(userlist)))
 
     def test_parse_parameters_and_filter_one_xml(self):
-        usersfile = os.path.join(MODULE_DIR, 'userlist.txt')
-        output_xml = os.path.join(MODULE_DIR, 'output.xml')
         expected_xml = os.path.join(MODULE_DIR, 'expected', 'filtered.xml')
         self.log_filter.parse_parameters_and_filter(['whodidwhat', '--input-xml', self.svn_xml,
-                                                     '--users-file', usersfile,
-                                                     '--output-xml', output_xml])
-        self.assertTrue(filecmp.cmp(output_xml, expected_xml))
+                                                     '--users-file', self.usersfile,
+                                                     '--output-xml', self.output_xml])
+        self.assertTrue(filecmp.cmp(self.output_xml, expected_xml))
+
+    @patch('whodidwhat.svnfilter.subprocess.check_output')
+    def test_read_log_from_svn_repo_output_xml(self, check_output_mock):
+        check_output_mock.return_value = self.svn_xml_text
+        svn_repos = os.path.join(MODULE_DIR, 'sample_data', 'svn_repos.txt')
+        self.log_filter.parse_parameters_and_filter(['whodidwhat', '--input-svn-repos', svn_repos,
+                                                     '--users-file', self.usersfile,
+                                                     '--output-xml', self.output_xml,
+                                                     '-r', '1234:HEAD'])
+        check_output_mock.assert_called_once_with(['svn', 'log', '-v', '--xml', '-r', '1234:HEAD',
+                                                   'https://svne1.access.nokiasiemensnetworks.com/isource/svnroot/ltetraining/python_intermediate/solutions'])
+
 
 
 if __name__ == "__main__":
--- whodidwhat/svnfilter.py
+++ whodidwhat/svnfilter.py
@@ -1,19 +1,18 @@
 import xml.etree.cElementTree as ET
 import sys
+import subprocess
 import argparse
+
 
 class SvnFilter(object):
 
-    def __init__(self):
-        pass
-
     def get_logs_by_users(self, xml_log, users):
-        tree = ET.parse(xml_log)
-        root = tree.getroot()
+        root = ET.fromstring(xml_log)
+        et = ET.ElementTree(element=root)
         for logentry in root.findall('logentry'):
             if logentry.find('author').text not in users:
                 root.remove(logentry)
-        return tree, root
+        return et, root
 
     def filter_logs_by_users(self, xml_log, userlist_file, outfile):
         userlist = self.read_userlist(userlist_file)
@@ -21,23 +20,41 @@ class SvnFilter(object):
         filtered_et.write(outfile, encoding='UTF-8', xml_declaration=True)
 
     def read_userlist(self, userlist_file):
-        userlist = []
-        for line in userlist_file:
+        return sorted(self._read_line_based_data(userlist_file))
+
+    def _read_line_based_data(self, fileobj):
+        data = []
+        for line in fileobj:
             if line.strip() and not line.strip().startswith('#'):
-                userlist.append(line.strip())
-        return sorted(userlist)
+                data.append(line.strip())
+        return data
 
     def parse_parameters_and_filter(self, argv=None):
         parameters = self.parse_parameters(argv)
-        self.filter_logs_by_users(parameters.input_xml,
+        input_xml = parameters.input_xml.read() if parameters.input_xml else self._get_xml_log(parameters)[0]
+        self.filter_logs_by_users(input_xml,
                                   parameters.users_file,
                                   parameters.output_xml)
+
+    def _get_xml_log(self, parameters):
+        repositories = self._read_line_based_data(parameters.input_svn_repos)
+        svn_log_texts = []
+        svn_command = ['svn', 'log', '-v', '--xml']
+        if parameters.revision:
+            svn_command.extend(['-r', parameters.revision])
+        for repository in repositories:
+            svn_command_with_repo = list(svn_command)
+            svn_command_with_repo.append('{}'.format(repository))
+            svn_log_texts.append(subprocess.check_output(svn_command_with_repo))
+        return svn_log_texts
 
     def parse_parameters(self, argv):
         argv = argv if argv is not None else sys.argv
 
-        parser = argparse.ArgumentParser('Filter svn and git repositories based on list of users'   )
+        parser = argparse.ArgumentParser('Filter svn and git repositories based on list of users')
         parser.add_argument('--input-xml', help='path to svn xml log input', type=file)
         parser.add_argument('--users-file', help='file of usernames given line-by-line', type=argparse.FileType('r'))
+        parser.add_argument('--input-svn-repos', help='file of svn repository paths given line-by-line', type=file)
         parser.add_argument('--output-xml', help='path for writing filtered xml')
+        parser.add_argument('-r', '--revision', help='revision info in similar format as svn log uses')
         return parser.parse_args(argv[1:])
