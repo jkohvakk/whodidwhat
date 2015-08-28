83dada04127393029114425aac35905aaa920e18
Janne Kohvakka <jkohvakk@gmail.com>
2015-08-28 14:02:44 +0300

    Committing get_common_paths script without any test. Not sure if I will be needing this one or not

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['mock'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -67,6 +67,7 @@ setup(
     entry_points={
         'console_scripts': [
             'whodidwhat=whodidwhat:whodidwhat',
+            'get_common_paths=whodidwhat:get_common_paths'
         ],
     },
 )
